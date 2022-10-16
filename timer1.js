bce5778f7415283361b1d55942d8dd42625d7a32
tronross <john_ross@analogforever.ca>
2022-10-16 14:19:08 -0400

    Code to extract alarm times from the CLI; began conditional logic to convert alarm times to ms expressed as numbers (converted from CLI string input).

---
+++ timer1.js
@@ -0,0 +1,31 @@
+// 'use strict';
+
+const times = process.argv.slice(2);
+let alarms = [];
+console.log(alarms);
+
+const alarmTimes = function(alarms) {
+  for (time of times) {
+    let alarms = parseInt(times, 10);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+process.stdout.write('\x07');
+
+// for (let j = 0; j < process.argv.length; j++) {
+//     console.log(j + ' -> ' + (process.argv[j]));
+// }
