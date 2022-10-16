f8ab391530ecf4a4f044bae4be357f5669bc4ceb
tronross <john_ross@analogforever.ca>
2022-10-16 15:45:21 -0400

    Added commentary, linted

--- timer1.js
+++ timer1.js
@@ -1,8 +1,11 @@
-const times = process.argv.slice(2);
+// Simple Timer : take input as presented to the CLI as arguments (interpreted as time in seconds) and beep after those times have elapsed.
 
+const times = process.argv.slice(2); // Extract pertinent arguments
+
+// alarmTimes: helper function translates input into timer alarms in ms, excluding negative (and non-)numbers.
 const alarmTimes = function(times) {
   let alarms = [];
-  for (time of times) {
+  for (let time of times) {
     let alarm = (parseInt(time, 10) * 1000);
     if (Number.isInteger(alarm) && alarm > 0) {
       alarms.push(alarm);
@@ -11,6 +14,7 @@ const alarmTimes = function(times) {
   return alarms;
 };
 
+//timer: implements timer which emits a beep as input timings elapse.
 const timer = function(times) {
   const alarms = alarmTimes(times);
   alarms.forEach(alarm => setTimeout(() => {
