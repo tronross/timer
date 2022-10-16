0aa249000f038e2c2c90f198b7faa0aab21c646c
tronross <john_ross@analogforever.ca>
2022-10-16 15:27:35 -0400

    Added conditional logic to exclude negative and non numbers

--- timer1.js
+++ timer1.js
@@ -4,19 +4,18 @@ const alarmTimes = function(times) {
   let alarms = [];
   for (time of times) {
     let alarm = (parseInt(time, 10) * 1000);
-    alarms.push(alarm);
+    if (Number.isInteger(alarm) && alarm > 0) {
+      alarms.push(alarm);
+    }
   }
   return alarms;
 };
 
 const timer = function(times) {
-  
   const alarms = alarmTimes(times);
-
   alarms.forEach(alarm => setTimeout(() => {
     process.stdout.write('\x07');
   }, alarm));
 };
-
 
 timer(times);
